17ebb4fbd6ce205b41a56391b9c53f7ea3da64fa
Mambafei Tanguy <mambafeitanguy@gmail.com>
2024-09-23 10:59:10 +0000

    Update du login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -14,9 +14,38 @@ export class LoginComponent {
   constructor(private apiService: ApiServiceService){}
   public responseData: any = null
   public errorMessage: String = ""
+  public successMessage: String = ""
   public formData = {
     email : "",
     password: ""
+  }
+
+  private validateForm():Boolean {
+    if (!this.formData.email || !this.formData.password) {
+      this.errorMessage = "Veillez renseigner tous les champs"
+      return false
+    }
+    return true
+  }
+
+  public login(){
+    if (!this.validateForm()) {
+      return
+    }
+
+    this.apiService.submitLoginUser(this.formData).subscribe({
+      next: (response) =>{
+        this.responseData = response
+        console.log(response)
+      },
+      error : (error)=>{
+        this.errorMessage= error.message
+        console.log(error)
+      },
+      complete: ()=>{
+        this.successMessage = "Connexion réussie"
+      }
+    })
   }
 
 }
--- src/app/services/api-service.service.ts
+++ src/app/services/api-service.service.ts
@@ -8,11 +8,16 @@ import { Observable } from 'rxjs';
 export class ApiServiceService {
 
   private registerUrl = "http://localhost:8080/register"
+  private loginUrl = "http://localhost:8080/login"
   private rolesUrl = "http://localhost:8080/roles"
   constructor(private http :HttpClient) { }
 
   submitRegisterUser(formData: any):Observable <any>{
     return this.http.post(this.registerUrl,formData);
+  }
+
+  submitLoginUser(formData:any): Observable <any>{
+    return this.http.post(this.loginUrl,formData);
   }
 
   getRoles():Observable <any>{
